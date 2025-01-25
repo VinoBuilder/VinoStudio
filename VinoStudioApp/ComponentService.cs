b6e0c70186a1a6c9faacf1dc6d5f16a24a071cae
Andrii Sych <97425977+SychAndrii@users.noreply.github.com>
2025-01-25 17:11:13 -0500

    Add component store interface and component service

---
+++ VinoStudioApp/ComponentService.cs
@@ -0,0 +1,27 @@
+﻿using VinoStudioCore.Stores;
+using IComponent = VinoStudioCore.Components.IComponent;
+
+namespace VinoStudioApp
+{
+    public class ComponentService
+    {
+        private readonly IComponentStore componentStore;
+
+        public ComponentService(IComponentStore componentStore)
+        {
+            this.componentStore = componentStore;
+        }
+        
+        public async Task<IComponent> AddComponent(IComponent component)
+        {
+            var c = await componentStore.AddComponent(component);
+            return c;
+        }
+        
+        public async Task<IComponent> GetComponent(Guid id)
+        {
+            var c = await componentStore.GetComponent(id);
+            return c;
+        }
+    }
+}
---
+++ VinoStudioCore/Stores/IComponentStore.cs
@@ -0,0 +1,10 @@
+﻿using VinoStudioCore.Components;
+
+namespace VinoStudioCore.Stores
+{
+    public interface IComponentStore
+    {
+        public Task<IComponent> AddComponent(IComponent component);
+        public Task<IComponent> GetComponent(Guid id);
+    }
+}
