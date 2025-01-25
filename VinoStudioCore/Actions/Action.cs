e5dbad707ddc8ac7b7976c3bf8144a7558a27836
Andrii Sych <97425977+SychAndrii@users.noreply.github.com>
2025-01-25 14:51:19 -0500

    Remove dialogue transmitter

--- VinoStudioCore/Actions/Action.cs
+++ VinoStudioCore/Actions/Action.cs
@@ -1,6 +1,18 @@
 ﻿namespace VinoStudioCore.Actions
 {
-    public interface IAction
+    public abstract class Action
     {
+        public TimeSpan Duration { get; private set; }
+
+        protected Action(TimeSpan duration)
+        {
+            Duration = duration;
+        }
+
+        public abstract Task StartExecute();
+        public virtual Task StopExecute()
+        {
+            return Task.CompletedTask;
+        }
     }
 }
--- VinoStudioCore/Actions/DialogueFillupAction.cs
+++ VinoStudioCore/Actions/DialogueFillupAction.cs
@@ -1,30 +1,56 @@
 ﻿using System;
+using System.Text;
+using VinoStudioCore.Components;
 
 namespace VinoStudioCore.Actions
 {
-    public class DialogueFillupAction : IAction
+    public class DialogueFillupAction : Action
     {
-        public string Value { get; set; } = "";
+        private readonly string value = "";
+        private readonly IDialogueComponent component;
 
-        private TimeSpan _duration;
-
-        public TimeSpan Duration
+        public static DialogueFillupAction Create(string value, TimeSpan duration, IDialogueComponent component)
         {
-            get => _duration;
-            set
+            if (duration < TimeSpan.Zero || duration > TimeSpan.FromSeconds(5))
             {
-                if (value < TimeSpan.Zero || value > TimeSpan.FromSeconds(5))
+                throw new ArgumentOutOfRangeException(nameof(duration), "Duration must be between 0 and 5 seconds.");
+            }
+
+            return new DialogueFillupAction(value, duration, component);
+        }
+
+        public override async Task StartExecute()
+        {
+            if (Duration == TimeSpan.Zero)
+            {
+                await component.SetText(value);
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder(value.Length);
+                int totalCharacters = value.Length;
+                int intervalMilliseconds = 50;
+                double totalMilliseconds = Duration.TotalMilliseconds;
+                int charactersPerInterval = (int)Math.Ceiling(totalCharacters / (totalMilliseconds / intervalMilliseconds));
+
+                for (int i = 0; i < totalCharacters; i += charactersPerInterval)
                 {
-                    throw new ArgumentOutOfRangeException(nameof(Duration), "Duration must be between 0 and 5 seconds.");
+                    sb.Append(value.Substring(i, Math.Min(charactersPerInterval, totalCharacters - i)));
+                    await component.SetText(sb.ToString());
+                    await Task.Delay(intervalMilliseconds);
                 }
-                _duration = value;
             }
         }
 
-        public DialogueFillupAction(string value, TimeSpan duration)
+        public override async Task StopExecute()
         {
-            Value = value;
-            Duration = duration;
+            await component.SetText(value);
+        }
+
+        private DialogueFillupAction(string value, TimeSpan duration, IDialogueComponent component) : base(duration)
+        {
+            this.value = value;
+            this.component = component;
         }
     }
 }
