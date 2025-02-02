3fb7198abeacfb3386cb16bae79d55890f9019ba
Andrii Sych <97425977+SychAndrii@users.noreply.github.com>
2025-02-02 15:10:38 -0500

    Add dialogue preview

---
+++ VinoStudioClient/Sections/Layout/ComponentPlacer/PreviewComponents/Dialogue/PreviewDialogueView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace VinoStudioClient.Sections.Layout.ComponentPlacer.PreviewComponents.Dialogue;
+
+public partial class PreviewDialogueView : UserControl
+{
+    public PreviewDialogueView()
+    {
+        InitializeComponent();
+    }
+}
